75007a373f3fc44f5f6c21695dc9bcc41ff1ee3a
Stephen Buchanan <stephen.buchanan@random.studio>
2022-04-15 12:06:41 +0200

    can view exported gltf model

--- app.js
+++ app.js
@@ -1,30 +1,101 @@
-import * as THREE from 'three';
+// three/examples/jsm/
 
-// init
+import * as THREE from "three";
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
+import { KTX2Loader } from "three/examples/jsm/loaders/KTX2Loader.js";
+import { MeshoptDecoder } from "three/examples/jsm/libs/meshopt_decoder.module.js";
+import { Vector3 } from "three";
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+let camera, scene, renderer;
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+init();
+render();
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
+function init() {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
 
-// animation
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild(renderer.domElement);
 
-function animation( time ) {
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
+  );
+  camera.position.set(0, 100, 0);
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+  const environment = new RoomEnvironment();
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
-	renderer.render( scene, camera );
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xbbbbbb);
+  scene.environment = pmremGenerator.fromScene(environment).texture;
 
+  const grid = new THREE.GridHelper(500, 10, 0xffffff, 0xffffff);
+  grid.material.opacity = 0.5;
+  grid.material.depthWrite = false;
+  grid.material.transparent = true;
+  scene.add(grid);
+
+  const ktx2Loader = new KTX2Loader()
+    .setTranscoderPath("js/libs/basis/")
+    .detectSupport(renderer);
+
+  const loader = new GLTFLoader().setPath("/");
+  loader.setKTX2Loader(ktx2Loader);
+  loader.setMeshoptDecoder(MeshoptDecoder);
+  loader.load("Lays_Jacket_LowPoly_A001.gltf", function (gltf) {
+    console.log("loaded:", gltf);
+    // coffeemat.glb was produced from the source scene using gltfpack:
+    // gltfpack -i coffeemat/scene.gltf -o coffeemat.glb -cc -tc
+    // The resulting model uses EXT_meshopt_compression (for geometry) and KHR_texture_basisu (for texture compression using ETC1S/BasisLZ)
+
+    // gltf.scene.position.y = 8;
+    const children = gltf.scene.children;
+    const target = children.find((c) => c.name === "High");
+    target.scale.x = 1;
+    target.scale.y = 1;
+    target.scale.z = 1;
+
+    console.log({ target });
+
+    scene.add(target);
+
+    render();
+  });
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render); // use if there is no animation loop
+  controls.minDistance = 400;
+  controls.maxDistance = 1000;
+  controls.target.set(10, 90, -16);
+  controls.update();
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
+  renderer.render(scene, camera);
 }
