fb898b2aa377dbf050448ad1595c4e1e2c3a7f43
Stephen Buchanan <stephen.buchanan@random.studio>
2022-05-08 13:20:43 +0200

    adding target for bone head

--- app.ts
+++ app.ts
@@ -8,8 +8,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 import { KTX2Loader } from "three/examples/jsm/loaders/KTX2Loader.js";
 import { MeshoptDecoder } from "three/examples/jsm/libs/meshopt_decoder.module.js";
-import { Vector3 } from "three";
-import { Object3D } from "three";
+import { Vector3, Object3D, Material } from "three";
 
 let camera, scene, renderer;
 
@@ -52,11 +51,9 @@ function init() {
 
   camera = new THREE.PerspectiveCamera(
     45,
-    window.innerWidth / window.innerHeight,
-    1,
-    2000
+    window.innerWidth / window.innerHeight
   );
-  camera.position.set(0, 100, 0);
+  camera.position.set(0, 0, -5);
 
   const environment = new RoomEnvironment();
   const pmremGenerator = new THREE.PMREMGenerator(renderer);
@@ -65,10 +62,10 @@ function init() {
   scene.background = new THREE.Color(0xbbbbbb);
   scene.environment = pmremGenerator.fromScene(environment).texture;
 
-  const grid = new THREE.GridHelper(500, 10, 0xffffff, 0xffffff);
-  grid.material.opacity = 0.5;
-  grid.material.depthWrite = false;
-  grid.material.transparent = true;
+  const grid = new THREE.GridHelper(10, 20, 0xffffff, 0xffffff);
+  (grid.material as Material).opacity = 0.5;
+  (grid.material as Material).depthWrite = false;
+  (grid.material as Material).transparent = true;
   scene.add(grid);
 
   const ktx2Loader = new KTX2Loader()
@@ -94,10 +91,10 @@ function init() {
     // });
 
     const rootObject = gltf.scene;
-    const uniformScale = 100;
-    rootObject.scale.x = uniformScale;
-    rootObject.scale.y = uniformScale;
-    rootObject.scale.z = uniformScale;
+    // const uniformScale = 100;
+    // rootObject.scale.x = uniformScale;
+    // rootObject.scale.y = uniformScale;
+    // rootObject.scale.z = uniformScale;
 
     // console.log({ rootObject });
     scene.add(rootObject);
@@ -109,26 +106,44 @@ function init() {
     //   .children.find((c) => c.name === "lower_arm_right");
     // // .children.find((c) => c.name === "trunk_right")
     // // .children.find((c) => c.name === "upper_arm_right");
-    const targetBone = findTargetBone(rootObject, "upper_arm_left");
+    const targetBone = findTargetBone(rootObject, "left_elbow-left_wrist");
+    // const targetBone = findTargetBone(rootObject, "left_wrist");
 
     if (targetBone) {
       console.log({ targetBone });
 
-      const geometry = new THREE.SphereGeometry(10, 32, 16);
-      const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-      const sphere = new THREE.Mesh(geometry, material);
-
       let tmpVector = new Vector3();
       const p = targetBone.getWorldPosition(tmpVector);
-      console.log({ p });
-      sphere.position.x = p.x;
-      sphere.position.y = p.y;
-      sphere.position.z = p.z;
-      scene.add(sphere);
+
+      console.log({ localPosition: targetBone.position, worldPosition: p });
+
+      const targets = [
+        {
+          name: "boneHead", // head is at the root of the bone
+          colour: 0xffff00,
+          position: {
+            x: p.x,
+            y: p.y,
+            z: p.z,
+          },
+        },
+      ];
+
+      targets.forEach((t) => {
+        const { colour, position } = t;
+        const geometry = new THREE.SphereGeometry(0.1, 32, 16);
+        const material = new THREE.MeshBasicMaterial({ color: colour });
+        const sphere = new THREE.Mesh(geometry, material);
+
+        sphere.position.x = position.x;
+        sphere.position.y = position.y;
+        sphere.position.z = position.z;
+        scene.add(sphere);
+      });
 
       setInterval(() => {
-        targetBone.rotation.x = Math.random() * 3;
-        // targetBone.position.x += 0.5 - Math.random();
+        // targetBone.rotation.x = Math.random() * 3;
+        // p.x += 0.5 - Math.random();
         render();
       }, 500);
     } else {
@@ -140,9 +155,9 @@ function init() {
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.addEventListener("change", render); // use if there is no animation loop
-  controls.minDistance = 400;
-  controls.maxDistance = 1000;
-  controls.target.set(10, 90, -16);
+  controls.minDistance = 1;
+  controls.maxDistance = 20;
+  controls.target.set(0, 0, 0);
   controls.update();
 
   window.addEventListener("resize", onWindowResize);
