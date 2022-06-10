a995c681438fd6a40d97c876d1dafd69ac78a29a
Stephen Buchanan <stephen.buchanan@random.studio>
2022-06-10 16:02:16 +0200

    weird workaround for getting version URL

--- PoseDetection.ts
+++ PoseDetection.ts
@@ -5,12 +5,14 @@ import * as posedetection from "@tensorflow-models/pose-detection";
 import { SupportedModels } from "@tensorflow-models/pose-detection";
 import { bonesMatchPose, drawPoseJoints, init, render } from "./Render3D";
 
+const version = mpPose.VERSION || "0.4.1633558788";
+
 async function createDetector() {
   return posedetection.createDetector(SupportedModels.BlazePose, {
     runtime: "mediapipe",
     // modelType: posedetection.movenet.modelType.MULTIPOSE_LIGHTNING,
     // modelType: "heavy",
-    solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}`,
+    solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${version}`,
   });
 }
 
