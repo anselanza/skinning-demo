78c41469d90657e0c392ac7ee96dbed0c913664d
Stephen Buchanan <stephen.buchanan@random.studio>
2022-06-10 15:56:04 +0200

    trying to fix build

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+import fs from "fs";
+import copy from "rollup-plugin-copy";
+
+export default {
+  plugins: [
+    mediapipe_workaround(),
+    copy({
+      targets: [
+        {
+          src: [
+            "./node_modules/@mediapipe/pose/*.wasm",
+            "./node_modules/@mediapipe/pose/*.tflite",
+            "./node_modules/@mediapipe/pose/*.binarypb",
+            "./node_modules/@mediapipe/pose/*wasm_bin.js",
+          ],
+          dest: "dist/",
+        },
+      ],
+    }),
+  ],
+};
+
+// https://github.com/google/mediapipe/issues/2883 が本家で対応されるまでのワークアラウンド
+function mediapipe_workaround() {
+  return {
+    name: "mediapipe_workaround",
+    load(id) {
+      if (path.basename(id) === "pose.js") {
+        let code = fs.readFileSync(id, "utf-8");
+        code += "exports.Pose = Pose;";
+        return { code };
+      } else {
+        return null;
+      }
+    },
+  };
+}
