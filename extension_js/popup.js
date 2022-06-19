2a8d410b8903b73cb375f4fcb7d20a842c26132f
Stephen Buchanan <stephen.buchanan@random.studio>
2022-06-19 13:25:26 +0200

    confirm screen capture starts

--- extension_js/popup.js
+++ extension_js/popup.js
@@ -4,12 +4,20 @@ const startButton = document.getElementById("startButton");
 // When the button is clicked, inject setPageBackgroundColor into current page
 startButton.addEventListener("click", async () => {
   console.log("clicked!");
+
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: startCapture,
+  chrome.desktopCapture.chooseDesktopMedia(["window"], tab, (streamId) => {
+    console.log("started desktopCapture OK");
+    //check whether the user canceled the request or not
+    if (streamId && streamId.length) {
+    }
   });
+
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: startCapture,
+  // });
 });
 
 // The body of this function will be executed as a content script inside the
