ce053de8ebf237a6a58f483c3deb2f762068a071
Stephen Buchanan <stephen.buchanan@random.studio>
2022-06-25 17:23:02 +0200

    this method works

--- popup.js
+++ popup.js
@@ -3,82 +3,114 @@ import { loadSystem } from "./PoseDetection";
 // Initialize button with user's preferred color
 const startButton = document.getElementById("startButton");
 
+// THIS IS THE KEY: https://stackoverflow.com/questions/29056607/in-chrome-extensions-is-it-possible-to-screencapture-and-display-the-screen-on
+// Using a Blob URL to stream the video from the Content Script context (which has access to the page/DOM)
+// to the Popup context (which has access to the scripts and libraries needed for pose detection and 3D rendering)
+
 // When the button is clicked, inject setPageBackgroundColor into current page
 startButton.addEventListener("click", async () => {
-  console.log("clicked!");
+  console.log("clicked! ready to get stream...");
 
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: startCapture,
-  });
-
-  chrome.desktopCapture.chooseDesktopMedia(["tab"], tab, async (streamId) => {
-    console.log("started desktopCapture OK", streamId);
-    //check whether the user canceled the request or not
-    if (streamId && streamId.length) {
-      console.warn("user cancelled request?");
+  try {
+    console.log("ask user for getDisplayMedia...");
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      audio: false,
+      video: true,
+    });
+    const videoElement = document.getElementById("sourceVideo");
+    if (videoElement) {
+      videoElement.srcObject = stream;
     }
+  } catch (e) {
+    console.error("could not get stream:", e);
+  }
 
-    setTimeout(() => {
-      console.log("send streamId now...");
-      chrome.tabs.sendMessage(tab.id, { streamId }, function (response) {
-        console.log("sending message with ", { streamId });
-        console.log({ response });
-      });
-    }, 1000);
-  });
+  // chrome.desktopCapture.chooseDesktopMedia(["tab"], (streamId) => {
+  //   console.log("got the streamId:", streamId);
+
+  //   chrome.tabs.sendMessage(tab.id, { streamId }, function (response) {
+  //     console.log({ response });
+
+  //   });
+  // });
+
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: startCapture,
+  // });
+
+  // chrome.desktopCapture.chooseDesktopMedia(["tab"], tab, async (streamId) => {
+  //   console.log("started desktopCapture OK", streamId);
+  //   //check whether the user canceled the request or not
+  //   if (streamId && streamId.length) {
+  //     console.warn("user cancelled request?");
+  //   }
+
+  //   setTimeout(() => {
+  //     console.log("send streamId now...");
+  //     chrome.tabs.sendMessage(tab.id, { streamId }, function (response) {
+  //       console.log("sending message with ", { streamId });
+  //       console.log({ response });
+  //     });
+  //   }, 1000);
+  // });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page. OUTPUT WILL BE IN THE CURRENT PAGE (not the popup inspector)!
 function startCapture() {
-  console.log("startCapture");
-  chrome.runtime.onMessage.addListener(async (message) => {
-    console.log("got message!", message);
-    const { streamId } = message;
-
-    try {
-      const mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: streamId,
-          },
-        },
-      });
-      console.log("got stream OK:", mediaStream);
-
-      const outputContainer = document.createElement("div");
-      outputContainer.style.position = "fixed";
-      outputContainer.style.top = 0;
-      outputContainer.style.left = 0;
-      outputContainer.style.border = "1px solid red";
-
-      const videoElement = document.createElement("video");
-      videoElement.srcObject = mediaStream;
-      videoElement.autoplay = true;
-      videoElement.style.position = "position";
-      videoElement.style.top = 0;
-      videoElement.style.left = 0;
-      videoElement.style.visibility = "hidden";
-
-      outputContainer.setAttribute("data-html2canvas-ignore", "true");
-
-      outputContainer.appendChild(videoElement);
-      console.log("append DOM elements", { videoElement, outputContainer });
-      document.body.appendChild(outputContainer);
-
-      console.log("...DOM appending done");
+  console.log("startCapture content script running OK");
+  chrome.runtime.onMessage.addListener(
+    async (message, sender, sendResponse) => {
+      console.log("got message from extension:", message, sender);
 
       try {
-        await loadSystem(outputContainer, videoElement);
-      } catch (poseSystemError) {
-        console.error("error starting pose detection system:", poseSystemError);
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            mandatory: {
+              chromeMediaSource: "desktop",
+              chromeMediaSourceId: streamId,
+            },
+          },
+        });
+        console.log("got stream OK:", mediaStream);
+
+        // const outputContainer = document.createElement("div");
+        // outputContainer.style.position = "fixed";
+        // outputContainer.style.top = 0;
+        // outputContainer.style.left = 0;
+        // outputContainer.style.border = "1px solid red";
+
+        // const videoElement = document.createElement("video");
+        // videoElement.srcObject = mediaStream;
+        // videoElement.autoplay = true;
+        // videoElement.style.position = "position";
+        // videoElement.style.top = 0;
+        // videoElement.style.left = 0;
+        // videoElement.style.visibility = "hidden";
+
+        // outputContainer.setAttribute("data-html2canvas-ignore", "true");
+
+        // outputContainer.appendChild(videoElement);
+        // console.log("append DOM elements", { videoElement, outputContainer });
+        // document.body.appendChild(outputContainer);
+
+        // console.log("...DOM appending done");
+
+        // try {
+        //   await loadSystem(outputContainer, videoElement);
+        // } catch (poseSystemError) {
+        //   console.error(
+        //     "error starting pose detection system:",
+        //     poseSystemError
+        //   );
+        // }
+        sendResponse();
+      } catch (streamError) {
+        console.error("error getting stream:", streamError);
       }
-    } catch (streamError) {
-      console.error("error getting stream:", streamError);
     }
-  });
+  );
 }
